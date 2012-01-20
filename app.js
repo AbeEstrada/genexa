e81692c1143ef082b52d5464c766a42e41a2eaa6
Abraham Estrada <abraham.estrada@gmail.com>
2012-01-20 13:29:41 -0700

    bootstrap + less

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var app = module.exports = express.createServer();
 
 
 app.configure(function(){
-    app.set('views', __dirname + '/views');
+    app.set('views', __dirname+'/views');
     app.set('view engine', 'jade');
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname+'/public'));
 });
 
 app.configure('development', function(){
