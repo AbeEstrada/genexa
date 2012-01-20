89211d57caa2564bbe8f74df6bf5fffca7033036
Abraham Estrada <abraham.estrada@gmail.com>
2012-01-20 13:02:02 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var routes = require('./routes');
+
+var app = module.exports = express.createServer();
+
+
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    })); 
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler()); 
+});
+
+
+app.get('/', routes.index);
+
+app.listen(3000);
+console.log('Listening on port %d in %s mode', app.address().port, app.settings.env);
