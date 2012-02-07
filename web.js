a9c99794d53e0ed5cdea69a87b80ee92dce95f8a
Abraham Estrada <abraham.estrada@gmail.com>
2012-02-06 18:26:28 -0700

    do not delete original zlib_bindings file

--- web.js
+++ web.js
@@ -3,13 +3,9 @@ var routes = require('./routes');
 
 var fs = require('fs');
 var util = require('util');
-
 var is = fs.createReadStream('./node_modules/pdfkit/node_modules/flate/build/Release/zlib_bindings.node')
 var os = fs.createWriteStream('./node_modules/pdfkit/node_modules/flate/lib/zlib_bindings.node');
-
-util.pump(is, os, function() {
-    fs.unlinkSync('./node_modules/pdfkit/node_modules/flate/build/Release/zlib_bindings.node');
-});
+util.pump(is, os);
 
 var app = module.exports = express.createServer();
 
