255a92a21331e439372a8795f0497d0a8071072d
Abraham Estrada <abraham.estrada@gmail.com>
2012-02-07 21:33:37 -0700

    adding questions to the generated pdf

--- routes/index.js
+++ routes/index.js
@@ -65,6 +65,7 @@ exports.create = function(req, res) {
 var create_pdf = function(data) {
     //console.log(data);
     var filename = data.name;
+    var fontSize = 12;
 
     var PDFDocument = require('pdfkit');
     var doc = new PDFDocument({
@@ -78,11 +79,38 @@ var create_pdf = function(data) {
     doc.font('Times-Roman').fontSize(12);
     doc.rect(80, 72, 80, 52).stroke();
     doc.fontSize(14).text(data.school, { align: 'center' });
-    doc.fontSize(12).text(data.subject, { align: 'center' });
-    doc.fontSize(12).text(data.teacher, { align: 'center' });
-    doc.fontSize(12).text(data.period, { align: 'center' });
+    doc.fontSize(fontSize).text(data.subject, { align: 'center' });
+    doc.fontSize(fontSize).text(data.teacher, { align: 'center' });
+    doc.fontSize(fontSize).text(data.period, { align: 'center' });
     doc.moveDown();
-    doc.fontSize(12).text('Nombre: ____________________________________   Grupo:__________'+'     '+data.date, { align: 'center' });
+    doc.fontSize(fontSize).text('Nombre: ____________________________________   Grupo:__________'+'     '+data.date, { align: 'center' });
+    doc.moveDown();
+    if (data.questions) {
+        for (var i = 0 ; i < data.questions.length; i++) {
+            var q = data.questions[i];
+            doc.moveDown();
+            doc.fontSize(fontSize).text((i+1)+'. '+q.question);
+            switch(q.type) {
+                case 'open':
+                    doc.moveDown(2);
+                    break;
+
+                case 'options':
+                    if (q.answers) {
+                        var answers = '';
+                        for (var j = 0; j < q.answers.length; j++) {
+                            answers += String.fromCharCode(65+j).toLowerCase()+') '+q.answers[j]+'  ';
+                        }
+                        doc.fontSize(fontSize).text(answers);
+                    }
+                    break;
+
+                case 'truefalse':
+                    doc.fontSize(fontSize).text('a) Verdadero  b) Falso');
+                    break;
+            }
+        }
+    }
 
     return doc.output();
 };
