5f08fdda5b67d05ec74b6591800b653774765c94
Abraham Estrada <abraham.estrada@gmail.com>
2012-02-10 10:28:12 -0700

    watermark

--- routes/index.js
+++ routes/index.js
@@ -85,6 +85,7 @@ var create_pdf = function(data) {
     });
 
     doc.font('./public/fonts/Times-New-Roman.ttf').fontSize(12);
+    doc.image('./public/img/watermark.png', 90, 50);
     doc.rect(80, 72, 80, 52).stroke();
     doc.fontSize(14).text(data.school, { align: 'center' });
     doc.fontSize(fontSize).text(data.subject, { align: 'center' });
